5bac5e4209ba26e4c2f415f960363108cbf0e289
Artyom Pugachev <artempugachev1@gmail.com>
2022-04-08 17:33:24 +0300

    redirect

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import './index.css';
 import App from './App';
 
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigate to={'/contracts'} replace />} />
+        <Route path="/contracts" element={<App />} />
+      </Routes>
+    </BrowserRouter>
+    
   </React.StrictMode>,
   document.getElementById('root')
 );
